3a78e64927fd8565bd46fbaf4450768b2eec81ae
MurcyLetch <arijitindu99@gmail.com>
2021-05-13 18:16:03 +0530

    prblm solving1

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
-import 'package:catalog/home_page.dart';
+import 'package:catalog/pages/home_page.dart';
+import 'package:catalog/pages/login_page.dart';
 import'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 void main(){
   runApp(MyApp());
@@ -11,7 +13,21 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: HomePage(),
+      //home: HomePage(),
+      themeMode: ThemeMode.light,
+      theme: ThemeData(
+        primarySwatch: Colors.deepPurple,
+        fontFamily: GoogleFonts.lato().fontFamily,
+        //primaryTextTheme: GoogleFonts.latoTextTheme(),
+      ),
+      darkTheme: ThemeData(
+        brightness: Brightness.dark,
+      ),
+      //initialRoute: "/home",//we can use anything which we want to show as the home page
+      routes: {
+       "/": (context)=>LoginPage() ,//HomePage(),
+        "/login":(context)=>LoginPage(),
+      },
     );
   }
 }
---
+++ lib/pages/login_page.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+
+class LoginPage extends StatelessWidget {
+  const LoginPage({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Material(
+      color: Colors.white,
+      child: Column(
+        children: [
+          Image.asset(
+            "assets/images/login_img.png",
+            fit: BoxFit.cover,
+          ),
+          SizedBox(
+            height: 20.0,
+          ),
+          Text(
+            "welcome",
+            style: TextStyle(
+              fontSize: 24,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+          SizedBox(
+            height: 20.0,
+          ),
+          
+
+        ],
+      ),
+    );
+  }
+}
