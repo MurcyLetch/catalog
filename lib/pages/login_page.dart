50544936cf66598a0dbdf143624570e3d1b29fda
MurcyLetch <arijitindu99@gmail.com>
2021-05-15 16:35:51 +0530

    day6_completed

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -11,98 +11,127 @@ class LoginPage extends StatefulWidget {
 class _LoginPageState extends State<LoginPage> {
   String name="";
   bool Changebutton=false;
-  
+  final _formkey=GlobalKey<FormState>();
+  movetohome(BuildContext context) async {
+    //after it returns true we can enter in to homepage
+      if(_formkey.currentState.validate()){
+        setState((){
+        Changebutton=true;                    
+        });
+        await Future.delayed(Duration(seconds:1));
+        await Navigator.pushNamed(context, MyRoutes.homeroute);
+        setState(() {
+        Changebutton=false;                    
+        });
+        }
+  }
   @override
   Widget build(BuildContext context) {
     return Material(
       color: Colors.white,
       child: SingleChildScrollView(
-        child: Column(
-          children: [
-            Image.asset(
-              "assets/images/login_img.png",
-              fit: BoxFit.cover,
-            ),
-            SizedBox(
-              height: 20.0,
-            ),
-            Text(
-              "welcome to MurcyDom, $name",
-              style: TextStyle(
-                fontSize: 25,
-                fontWeight: FontWeight.bold,
+        child: Form(
+          key: _formkey,
+          child: Column(
+            children: [
+              Image.asset(
+                "assets/images/login_img.png",
+                fit: BoxFit.cover,
               ),
-            ),
-            SizedBox(
-              height: 20.0,
-            ),
-          Padding(
-            padding: const EdgeInsets.symmetric(vertical: 16.0,horizontal: 32.0),
-            child: Column(
-              children: [
-              TextFormField(
-                decoration:InputDecoration(
-                  hintText: "Enter User Name",
-                  labelText: "Username",
-                ),
-                onChanged: (value){
-                  name=value;
-                  setState(() {});//it refreshes the page by calling build method..it does not work for stateless widget
-                },
-              ),
-
-              TextFormField(
-                obscureText: true,
-                decoration:InputDecoration(
-                  hintText: "Type your password",
-                  labelText: "Password",
-                ),
-              ),
-
               SizedBox(
-              height: 45.0,
-             ),
-              
-              //in Inkwell we made the elevator button manually
-              InkWell(      //we can use gesture inplace of Inkwell for clickable propertry..but in gesture there will be nothing which will shoe that it is clicking
-                onTap: () async {
-                  setState(() {
-                  Changebutton=true;                    
-                  });
-                  await Future.delayed(Duration(seconds:1));
-                      Navigator.pushNamed(context, MyRoutes.homeroute);
-                },
-                child: AnimatedContainer(
-                  duration: Duration(seconds : 1),
-                  height:40,
-                  width:Changebutton? 75 : 150,
-                  //color: Colors.deepPurple,
-                  alignment: Alignment.center,
-                  child: Changebutton?Icon(Icons.done,color:Colors.white) :Text(
-                    "Log In",
-                    style: TextStyle(
-                      color: Colors.white,
-                      fontWeight: FontWeight.bold,
-                      fontSize: 18,
+                height: 20.0,
+              ),
+              Text(
+                "welcome to MurcyDom, $name",
+                style: TextStyle(
+                  fontSize: 25,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              SizedBox(
+                height: 20.0,
+              ),
+            Padding(
+              padding: const EdgeInsets.symmetric(vertical: 16.0,horizontal: 32.0),
+              child: Column(
+                children: [
+                TextFormField(
+                  decoration:InputDecoration(
+                    hintText: "Enter User Name",
+                    labelText: "Username",
+                  ),
+                  // ignore: missing_return
+                  validator: (value){
+                    if(value.isEmpty){
+                      return "username can not be empty";
+                    }
+
+                    return null;
+                  },
+                  onChanged: (value){
+                    name=value;
+                    setState(() {});//it refreshes the page by calling build method..it does not work for stateless widget
+                  },
+                ),
+
+                TextFormField(
+                  obscureText: true,
+                  decoration:InputDecoration(
+                    hintText: "Type your password",
+                    labelText: "Password",
+                  ),
+                  //validator portion....
+                  validator: (value){
+                    if(value.isEmpty){
+                      return "password is mandatory";
+                    }
+                    else if(value.length<6){
+                      return "password length should be atleast 6";
+                    }
+
+                    return null;
+                  },
+                ),
+
+                SizedBox(
+                height: 45.0,
+               ),
+                
+                //in Inkwell we made the elevator button manually
+                Material(
+                  color: Colors.deepPurple,
+                  borderRadius: BorderRadius.circular(Changebutton?20 : 10),
+                  child: InkWell(      //we can use gesture inplace of Inkwell for clickable propertry..but in gesture there will be nothing which will shoe that it is clicking
+                    onTap: () =>movetohome(context),
+                    child: AnimatedContainer(
+                      duration: Duration(seconds : 1),
+                      height:40,
+                      width:Changebutton? 75 : 150,
+                      //color: Colors.deepPurple,
+                      alignment: Alignment.center,
+                      child: Changebutton?Icon(Icons.done,color:Colors.white) :Text(
+                        "Log In",
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontWeight: FontWeight.bold,
+                          fontSize: 18,
+                        ),
+                      ),
                     ),
                   ),
-                  decoration: BoxDecoration(
-                    color: Colors.deepPurple,
-                    borderRadius: BorderRadius.circular(Changebutton?20 : 10),
-                  ),
                 ),
+                // ElevatedButton(
+                //   child: Text("Log In"),
+                //   style: TextButton.styleFrom(minimumSize: Size(125,40)),
+                //   onPressed: (){
+                //     Navigator.pushNamed(context, MyRoutes.homeroute);
+                //   },
+                //   ),   
+                ],
               ),
-              // ElevatedButton(
-              //   child: Text("Log In"),
-              //   style: TextButton.styleFrom(minimumSize: Size(125,40)),
-              //   onPressed: (){
-              //     Navigator.pushNamed(context, MyRoutes.homeroute);
-              //   },
-              //   ),   
-              ],
-            ),
-          )
-          ],
+            )
+            ],
+          ),
         ),
       ),
     );
